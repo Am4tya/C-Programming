4da562752cc11ffea929394b8a00e95b13c44ce1
AmtSahil_47 <51235342+AmtSahil@users.noreply.github.com>
2021-06-25 22:04:26 +0545

    Add files via upload

---
+++ 16 TernaryOperator.c
@@ -0,0 +1,20 @@
+/* Ternary Operator
+   syntax conditition? statement if true : statement if false.
+*/ 
+#include <stdio.h>
+
+int main(){
+	
+	int age;
+	printf("Enter your age: \n");
+	scanf("%d",&age);
+	
+	age >22 ? printf("You are Selected.") : printf("You are not selected.");
+	
+	
+	return 0;
+}
+
+
+
+
