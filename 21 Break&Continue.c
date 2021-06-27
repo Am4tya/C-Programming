7af26b38989cf06a782f80be6c5cae9581fa7c1b
AmtSahil_47 <51235342+AmtSahil@users.noreply.github.com>
2021-06-27 17:55:14 +0545

    Add files via upload

---
+++ 21 Break&Continue.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+// Break: Break the entire Loop ; Continue: Bypass
+int main(){
+	
+int i;
+for(i=1 ; i<=100 ; i++){
+	if(i==10)
+	continue;
+	printf("%d\n",i);
+}
+		   
+	return 0;
+}
